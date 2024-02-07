fb9f529ea072793cfa3a1bdd9f5b2872705221c7
Aarn Rast <aarnav.rastogi@gmail.com>
2024-02-07 12:29:26 +0530

    ok

--- src/pages/jobs/components/CardGrid.js
+++ src/pages/jobs/components/CardGrid.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from "react";
 import CustomCard from "../../../shared/Cards/CustomCardJob";
-import FilterSortCards from '../containers/FilterSortCards'; 
+import FilterSortCards from "../containers/FilterSortCards";
+import { customToast } from "../../../utils/helper";
 
 const CardGrid = ({ jobDetails }) => {
-  const sortedAndFilteredCards = FilterSortCards({ jobDetails });
-
-  return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols justify-center gap-4 p-4 pt-4 overflow-scroll">
-      {sortedAndFilteredCards.map((card, index) => (
-        <CustomCard
-          key={index}
-          jobLogo={card.jobLogo}
-          companyLogo={card.companyLogo}
-          companyName={card.companyName}
-          jobTitle={card.jobTitle}
-          salary={"₹" + card.salary + "L/Yr"}
-          jobDesc={card.jobDesc}
-        />
-      ))}
-    </div>
-  );
+   const sortedAndFilteredCards = FilterSortCards({ jobDetails });
+   if (sortedAndFilteredCards.length === 0) customToast("No Jobs Found", 'alert')
+   return (
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols justify-center gap-4 p-4 pt-4 overflow-scroll">
+         {sortedAndFilteredCards.map((card, index) => (
+            <CustomCard
+               key={index}
+               jobLogo={card.jobLogo}
+               companyLogo={card.companyLogo}
+               companyName={card.companyName}
+               jobTitle={card.jobTitle}
+               salary={"₹" + card.salary + "L/Yr"}
+               jobDesc={card.jobDesc}
+            />
+         ))}
+      </div>
+   );
 };
 
 export default CardGrid;
