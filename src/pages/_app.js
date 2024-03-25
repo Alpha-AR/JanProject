d7409ac4e773ce97f7dcf682a0090c2bf4586387
AarnRast1 <aarnav.rastogi@ongrid.in>
2024-03-26 02:40:31 +0530

    kuch toh karr raha tha

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,53 +1,61 @@
-import AppContext from "../contexts/AppContext";
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUsername } from "../utils/redux/actions";
 import { motion, AnimatePresence } from "framer-motion";
-import React, { useState, useEffect } from "react";
 import { Header, Footer } from "../shared";
-import { useRouter } from "next/router";
 import { Toaster } from "react-hot-toast";
 import "../assets/styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../utils/redux/store";
 
-export default function App({ Component, pageProps }) {
-   const [userName, setUserName] = useState(null);
-   const router = useRouter();
-   useEffect(() => {
-      const storedCredentials = localStorage.getItem("credentials");
-      if (storedCredentials) {
-         const parsedCredentials = JSON.parse(storedCredentials);
-         if (parsedCredentials.isLoggedIn === 1) {
-            setUserName(parsedCredentials.name);
-         }
-      }
-   }, []);
-   return (
-      <>
-         <AppContext.Provider value={{ userName, setUserName }}>
-            <AnimatePresence mode="wait">
-               <motion.div key={router.pathname}>
-                  <div className="flex flex-col min-h-screen">
-                     <Header text={userName} />
-                     <div className="flex-1 bg-gradient-to-tr from-pink-50 to-white ">
-                        <Component {...pageProps} />
+class App extends React.Component {
+   componentDidMount() {
+      this.props.fetchUsername();
+   }
+
+   render() {
+      const { userName, Component, pageProps } = this.props;
+
+      return (
+         <>
+            <Provider store={store}>
+               <AnimatePresence mode="wait">
+                  <motion.div key={router.pathname}>
+                     <div className="flex flex-col min-h-screen">
+                        <Header text={userName} />
+                        <div className="flex-1 bg-gradient-to-tr from-pink-50 to-white ">
+                           <Component {...pageProps} />
+                        </div>
+                        <Toaster position="bottom" />
+                        <Footer />
                      </div>
-                     <Toaster position="bottom" />
-                     <Footer />
-                  </div>
-                  <motion.div
-                     className= "absolute top-0 left-0 w-[100%] h-[100vh] bg-gradient-to-r from-blue-200 to-cyan-200 origin-bottom"
-                     initial={{ scaleY: 0 }}
-                     animate={{ scaleY: 0 }}
-                     exit={{ scaleY: 1 }}
-                     transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
-                  ></motion.div>
-                  <motion.div
-                     className= "absolute top-0 left-0 w-[100%] h-[100vh] bg-gradient-to-r from-blue-200 to-cyan-200 origin-top"
-                     initial={{ scaleY: 1 }}
-                     animate={{ scaleY: 0 }}
-                     exit={{ scaleY: 0 }}
-                     transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
-                  ></motion.div>
-               </motion.div>
-            </AnimatePresence>
-         </AppContext.Provider>
-      </>
-   );
+                     <motion.div
+                        className="absolute top-0 left-0 w-[100%] h-[100vh] bg-gradient-to-r from-blue-200 to-cyan-200 origin-bottom"
+                        initial={{ scaleY: 0 }}
+                        animate={{ scaleY: 0 }}
+                        exit={{ scaleY: 1 }}
+                        transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+                     ></motion.div>
+                     <motion.div
+                        className="absolute top-0 left-0 w-[100%] h-[100vh] bg-gradient-to-r from-blue-200 to-cyan-200 origin-top"
+                        initial={{ scaleY: 1 }}
+                        animate={{ scaleY: 0 }}
+                        exit={{ scaleY: 0 }}
+                        transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
+                     ></motion.div>
+                  </motion.div>
+               </AnimatePresence>
+            </Provider>
+         </>
+      );
+   }
 }
+const mapStateToProps = (state) => ({
+   userName: state.userName,
+});
+
+const mapDispatchToProps = {
+   fetchUsername,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
